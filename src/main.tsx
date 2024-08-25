73e41727a50c402115f718b4fda574cfde84aacd
Mahomed Cissokho <mahomedcissokho@esp.sn>
2024-08-25 23:13:22 +0000

    services module hardely et fully developped with responsivity, precision and eys-tracking based on the design

--- src/main.tsx
+++ src/main.tsx
@@ -32,6 +32,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 							
 							<Route path="/*" element={<Home />} />
 
+
 						</Routes>
 					</BrowserRouter>
 				</ModalProvider>
