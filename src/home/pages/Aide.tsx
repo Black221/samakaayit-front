ba31c77b9c49078fd2c1821ded6fce232bc25ea9
Mahomed Cissokho <mahomedcissokho@esp.sn>
2024-08-31 09:39:24 +0000

    add configurations route about help page then create and implemente First Section for Help Page

---
+++ src/home/pages/Aide.tsx
@@ -0,0 +1,15 @@
+
+function Aide() {
+  return (
+    <>
+      <section className="text-white h-auto bg-[#085F28] py-20 lg:py-32 mb-24 flex items-center flex-col gap-5">
+        <h1 className="text-3xl font-bold ">Besoin d'aide ?</h1>
+        <p className="text-center  md:w-[37rem] text-pretty">
+          Si vous avez besoin d’une aide quelconque, répondez à notre FAQ et faites nous en part.
+        </p>
+      </section>
+    </>
+  );
+}
+
+export default Aide;
